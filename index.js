55a5d531e22fabbf9e130dbd43322a756d3b62a4
BalramMardi <120470787+BalramMardi@users.noreply.github.com>
2025-07-22 11:35:58 +0530

    220725-2 routes created

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import contactRoutes from "./routes/contact.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+
+app.use("/", contactRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ routes/contact.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/identify", identify);
+
+export default router;
